475620d410c2faefb85b6ab20c45dcdbd90ccfc1
CarlsKe <fernancarldiaz@outlook.com>
2018-07-23 15:25:28 -0500

    feat: initials fields

---
+++ README.md
@@ -0,0 +1,27 @@
+# webcomponents-hero
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+## Prerequisites
+
+Install [GULP](https://gulpjs.com/) using
+[yarn](https://yarnpkg.com/lang/en/) (we assume you have pre-installed [node.js](https://nodejs.org)).
+   
+## Start the development server
+
+This command serves the app at `http://127.0.0.1:8081` and provides basic URL
+routing for the app:
+
+    ```
+    $ gulp
+    
+    ```
+
+
+## Built With
+
+* [GULP](https://gulpjs.com/)
+* [NPM](https://www.npmjs.com/)
+* [SCSS](https://sass-lang.com/)
---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+
+gulp.task('connect', ()=>{
+  connect.server({
+    root: 'build',
+    livereload: true
+  });
+});
+
+
+gulp.task('html', function(){
+  return gulp.src('src/templates/**/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('./build/templates'))
+    .pipe(connect.reload());
+  });
+
+  gulp.task('htmlHome', function(){
+    return gulp.src('./src/index.pug')
+      .pipe(pug())
+      .pipe(gulp.dest('./build'))
+      .pipe(connect.reload());
+    });
+
+  
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/**/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./build/css'))
+    .pipe(connect.reload());
+
+  });
+
+
+gulp.task('js',()=>{
+  gulp.src('./src/js/*.js')
+      .pipe(gulp.dest('./build/js'))
+      .pipe(connect.reload());
+});
+
+  
+gulp.task('img',()=>{
+  gulp.src('./src/img/**')
+      .pipe(gulp.dest('./build/img'))
+      .pipe(connect.reload());
+});
+
+gulp.task('fonts',()=>{
+  gulp.src('./src/fonts/**/*.ttf')
+      .pipe(gulp.dest('./build/fonts'))
+      .pipe(connect.reload());
+});
+
+
+gulp.task('normalize',()=>{
+  gulp.src('./node_modules/normalize.css/*.css')
+      .pipe(gulp.dest('./build/css'))
+      .pipe(connect.reload());
+});
+
+
+gulp.task('watch', function () {
+  gulp.watch('./src/sass/**/*.sass', ['sass']);
+  gulp.watch('./src/templates/*.pug',['html']);
+  gulp.watch('./src/js/**/*.js',['js']);
+  gulp.watch('./src/img/**/*.jpeg',['img']);
+
+});
+
+
+gulp.task('default', ['html','htmlHome','sass','js',
+   'img','fonts','normalize','watch' ,'connect' ]);
